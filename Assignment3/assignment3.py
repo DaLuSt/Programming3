b5dd2e71ab3ff4fb738305cf199793f89f67a817
DaLuSt <daan_lucas@live.nl>
2022-05-24 14:50:30 +0200

    assign 3 start, readme fix, small cleanup

---
+++ Assignment3/assignment3.py
@@ -0,0 +1,36 @@
+"""
+Assignment 3 Programming 3
+Data Sciences for Life Sciences
+Author: Daan Steur
+"""
+
+# MapReduce jobs. The jobs are represented as a python script, which implements 3 methods as an interface:
+
+# An iterable data object called "Data"
+# A function called "mapper" which takes (inkey, invalue) tuples
+# A function called "reducer" which takes (outkey, intermediatevalue) tuples.
+
+
+class MapReduce:
+    
+    def __init__(self):
+        
+        
+    
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# command line assignment3.py –hosts <list of hosts, first is server> [-s | -c] -n cores -p port -d jobsdirectory -r <retries>
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Data science for life sciences programming 3 course, weekly assignments and fina
 
 - week 1 assignment (download pmid citations as xml)
 - week 2 assignment (Download and Process Pubmed Abstracts from Multiple Hosts)
-- week 3 assignment
+- week 3 assignment (Run BLASTP via SLURM)
 - week 4 assignment
 - week 5 assignment
 - final assignment
