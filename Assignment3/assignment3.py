06470de54d54c6db78efb4c02037c0f7bfc2dc6b
DaLuSt <daan_lucas@live.nl>
2022-05-29 20:54:03 +0200

    start

--- Assignment3/assignment3.py
+++ Assignment3/assignment3.py
@@ -11,6 +11,21 @@ Author: Daan Steur
 # A function called "reducer" which takes (outkey, intermediatevalue) tuples.
 
 
+#!/bin/bash
+#SBATCH --time 2:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=16
+module load Bowtie2
+export BOWTIE2_INDEXES=/data/p225083/BOWTIE2_INDEXES
+export DATA=/data/p225083
+bowtie2 -x human -U$DATA/all.fq -p 16 -S${DATA}/output.sam 
+
+#SBATCH –mail-user=(mail adress) will run the script and sent you updates.
+#SBATCH –outpu=somefile /
+
+
+# The "x" isavariable, which is set consecutively from1to 10
+For x in {1..16} ; do something ; done
 
         
         
--- Assignment3/assignment3.sh
+++ Assignment3/assignment3.sh
@@ -2,14 +2,26 @@
 #SBATCH --time 2:00:00
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=16
-module load Bowtie2
-export BOWTIE2_INDEXES=/data/p225083/BOWTIE2_INDEXES
-export DATA=/data/p225083
-bowtie2 -x human -U$DATA/all.fq -p 16 -S${DATA}/output.sam 
+#SBATCH --partition=short
+#SBATCH --mem=size_in_MB
+#SBATCH --output=somefile
 
-#SBATCH –mail-user=(mail adress) will run the script and sent you updates.
-#SBATCH –outpu=somefile /
+# run with -num_threads from 1 to 16 on query file MCRA.faa
+export BLASTDB=/local-fs/datasets/
+for x in {1..16} ; do time blastp -query MCRA.faa -db refseq_protein/refseq_protein -num_threads $x -outfmt 6 >> timings.txt ; done
 
+# capture time in seconds of runtime in a file
+# called timings.txt in output directory
+# based on linux time command
+# /usr/bin/time
 
-# The "x" isavariable, which is set consecutively from1to 10
-For x in {1..16} ; do something ; done
+# timings.png file of matplotlib plot
+# x-axis: number-if-threads
+# y-axis: time taken (s)
+
+# Make sure the python executable is in your PATH environment
+# variable then add in your script
+# python path/to/the/python_script.py
+
+#!/bin/sh
+python python_script.py
