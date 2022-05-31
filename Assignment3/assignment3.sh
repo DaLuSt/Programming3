c065ab33a8b7b9e2a454e3de0f9b405c5c0c6ce8
Daan Steur <75691752+DaLuSt@users.noreply.github.com>
2022-05-31 13:53:17 +0200

    grammer

--- Assignment3/assignment3.sh
+++ Assignment3/assignment3.sh
@@ -3,7 +3,7 @@
 #SBATCH --time 7:00:00
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=16
-#SBATCH --job-name=BlastpPeterFinal
+#SBATCH --job-name=BlastpDLS
 #SBATCH --partition=assemblix
 
 mkdir -p output
