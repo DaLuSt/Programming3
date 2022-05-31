7ad8c410bb9fe90fdb5dc53c560fd01fd3a9a072
DaLuSt <daan_lucas@live.nl>
2022-05-31 13:23:25 +0200

    cleanup

--- Assignment3/assignment3.sh
+++ Assignment3/assignment3.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
+#!/bin/bash
+#SBATCH --time 7:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=16
+#SBATCH --job-name=BlastpPeterFinal
+#SBATCH --partition=assemblix
 
-mkdir -p  output
-echo ' ' > output/time.txt
-
-blast() {
-	export BLASTDB=/local-fs/datasets/
-	blastp -query MCRA.faa -db refseq_protein/refseq_protein -num_threads $1 -outfmt 6 >> blastoutput.txt
-    }
-
-for i in {1..16}
-do
-echo '------------' >> output/timings.txt
-echo "threads $i" >> output/timings.txt
-(time blast $i) 2>> output/timings.txt
-done
-
+mkdir -p output
+export BLASTDB=/local-fs/datasets/refseq_protein/refseq_protein
+export time=/usr/bin/time
+export time_file=output/timings.txt
+for n in {1..16}; do $time --append -o $time_file -f "${n}\t%e" blastp -query MCRA.faa -db $BLASTDB -num_threads $n -outfmt 6; done
 python3 time_plot.py
--- Assignment3/time_plot.py
+++ Assignment3/time_plot.py
@@ -3,33 +3,23 @@ Assignment 3 Programming 3
 Data Sciences for Life Sciences
 Author: Daan Steur
 """
-
+import pandas as pd
 import matplotlib.pyplot as plt
 
-with open('output/time.txt') as f:
-    lines = f.readlines()
-
-thread_list = []
-time_list = []
-
-for line in lines:
-    print(line)
-    if (line.startswith('threads')):
-        thread_list.append(int(line.split(' ')[1].split('\n')[0]))
-
-    if (line.startswith('real')):
-        time = ((line.split('	')[1].split('\n')[0]))
-        time = float(time.split('m')[0]) * 60 + float(time.split('m')[1].split('s')[0])
-        time_list.append(float(time))
-
-print(thread_list)
-print(time_list)
-
-plt.bar(thread_list, time_list, color='b', width = 1)
-plt.xlabel('CPU threads')
-plt.ylabel('Time (s)')
-plt.title('Time taken to run the program')
-plt.savefig('output/time_plot.png')
+data = pd.read_csv(
+    "/homes/pcriesebos/Documents/programming3/Assignment3/output/timings.txt",
+    sep="\t",
+    lineterminator="\n",
+    header=None,
+)
+x = data.iloc[:, 0]
+y = data.iloc[:, 1]
+plt.scatter(x, y)
+plt.plot(x, y)
+plt.title("Blastp runtime with variating thread count")
+plt.xlabel("Number of threads")
+plt.ylabel("Time taken (s)")
+plt.savefig("output/timings.png")
         
         
     
