9a0090ea51dd803320dc91d1f848b34fc503078c
DaLuSt <daan_lucas@live.nl>
2022-05-31 10:30:17 +0200

    code fix

--- Assignment3/assignment3.py
+++ Assignment3/assignment3.py
@@ -4,28 +4,17 @@ Data Sciences for Life Sciences
 Author: Daan Steur
 """
 
-# MapReduce jobs. The jobs are represented as a python script, which implements 3 methods as an interface:
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-# An iterable data object called "Data"
-# A function called "mapper" which takes (inkey, invalue) tuples
-# A function called "reducer" which takes (outkey, intermediatevalue) tuples.
+data = pd.read_csv('timings.txt', sep="", header=None)
+y = data.time
+x = np.linspace(1, 17)
+plt.plot(x, y)
 
-
-#!/bin/bash
-#SBATCH --time 2:00:00
-#SBATCH --nodes=1
-#SBATCH --cpus-per-task=16
-module load Bowtie2
-export BOWTIE2_INDEXES=/data/p225083/BOWTIE2_INDEXES
-export DATA=/data/p225083
-bowtie2 -x human -U$DATA/all.fq -p 16 -S${DATA}/output.sam 
-
-#SBATCH –mail-user=(mail adress) will run the script and sent you updates.
-#SBATCH –outpu=somefile /
-
-
-# The "x" isavariable, which is set consecutively from1to 10
-For x in {1..16} ; do something ; done
+# load timings.txt
+# export timings.png
 
         
         
