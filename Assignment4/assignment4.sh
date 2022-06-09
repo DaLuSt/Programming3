936fd37b924ae6e39672e148f7883a26e82230ea
DaLuSt <daan_lucas@live.nl>
2022-06-09 14:26:15 +0200

    cleanup

--- Assignment4/assignment4.sh
+++ Assignment4/assignment4.sh
@@ -15,4 +15,4 @@ mkdir -p /students/2021-2022/master/DaanSteur_DSLS/output
 mkdir -p output
 
 # Run velveth and store output in student folder 
-seq 25 2 27 | parallel -j16 'velveth $OUTPUT/{} {} -longPaired -fastq $FILE1 $FILE2 && velvetg $OUTPUT/{} && cat $OUTPUT/{}/contigs.fa | (python3 assignment4.py && echo -e Kmer_size:{}; ) >> output/output.csv'
+seq 25 2 31 | parallel -j16 'velveth $OUTPUT/{} {} -longPaired -fastq $FILE1 $FILE2 && velvetg $OUTPUT/{} && cat $OUTPUT/{}/contigs.fa | (python3 assignment4.py && echo -e Kmer_size:{}; ) >> output/output.csv'
