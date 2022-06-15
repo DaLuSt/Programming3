41b7ea55e402ba15b076552c6e2609daa214241c
DaLuSt <daan_lucas@live.nl>
2022-06-15 16:51:17 +0200

    pseudo code anwsers.

--- Assignment5/assignment5.py
+++ Assignment5/assignment5.py
@@ -8,6 +8,8 @@ Date:13/06/2022
 
 # packages
 from email import header
+
+from sympy import bottom_up
 from pyspark import SparkFiles
 from pyspark import SparkContext
 from pyspark.sql import Row
@@ -39,7 +41,7 @@ def spark_df(path):
 
 
 
-class InspectInterPRO:
+class InspectSparkDF:
 
     def __init__(self, spark_df):
         self.df = spark_df
@@ -47,51 +49,56 @@ class InspectInterPRO:
 
     def unique_pro_annots(self):
         return self.df.select('interPRO_accession').distinct().count()
-        
-
 # 1. How many distinct protein annotations are found in the dataset? I.e. how many distinc InterPRO numbers are there?
 # df[column].unique()
 
 
     def avg_pro_annots(self):
-
-# 2. How many annotations does a protein have on average?
-# df[column].mean()
-        return self.df["protein_accession"].mean()
+        # 2. How many annotations does a protein have on average?
+        avg = self.df.select('interPRO_accession').distinct().count() / self.df.count()
+        return avg
 
 
     def max_go_term(self):
-# 3. What is the most common GO Term found?
+        # 3. What is the most common GO Term found?
+        max = self.df.select('GO_annots').distinct().count().max()
+        return max
 # argmax(df[column]) / # df[column].sum().max()
         pass
 
 
     def avg_feature_size(self):
-# 4. What is the average size of an InterPRO feature found in the dataset?
+        # 4. What is the average size of an InterPRO feature found in the dataset?
+
+        
+        
 # df[column].mean() > COMBINE WITH Q2 INTO 1 FUNCITON,S THEN CREATE AN EXTRA FUNCTION
 # TO ANSWER THE ASSIGNMENT QUESTIONS
         pass
 
 
-    def top_10_features(self):
-# 5. What is the top 10 most common InterPRO features?
+    def top_10_common_features(self):
+        # 5. What is the top 10 most common InterPRO features?
+        top_10 = self.df.select('interPRO_accession').distinct().count().top(10)
 # df.value_counts(ascending=True)[0:10]
         pass
 
 
     def top_10_percent(self):
-# 6. If you select InterPRO features that are almost the same size (within 90-100%) as the protein itself, what is the top10 then?
+        # 6. If you select InterPRO features that are almost the same size (within 90-100%) as the protein itself, what is the top10 then?
+        top_10_percent = self.df.select('interPRO_accession').distinct().count().top(10) / self.df.count()
 # top10percent = len(df)*0.1 > top10percent_df = df.iloc[length > top10percent] > df.value_counts(ascending=True)[0:10]
         pass
 
 
-    # def COMBINEQ5Q6
+    def COMBINEQ5Q6
 # 7. If you look at those features which also have textual annotation, what is the top 10 most common word found in that annotation?
 # df.value_counts(ascending=True)[0:10]
 
 
-    def top_10_features(self):
-# 8. And the top 10 least common?
+    def top_10_least_common_features(self):
+        # 8. And the top 10 least common?
+        bottom_10 = self.df.select('interPRO_accession').distinct().count().bottom(10)
 # df.value_counts(ascending=False)[0:10] / df.value_counts(ascending=True)[-10:]
         pass
 
@@ -102,7 +109,8 @@ class InspectInterPRO:
 
 
     def cor_coef(self):
-# 10. What is the coefficient of correlation ($R^2$) between the size of the protein and the number of features found?
+        # 10. What is the coefficient of correlation ($R^2$) between the size of the protein and the number of features found?
+        coef = self.df.select('seq_length').corr(self.df.select('interPRO_accession').distinct().count())
 # coeffcor = (stuff)^2
         pass
 
@@ -117,7 +125,7 @@ class InspectInterPRO:
 if __name__ == "__main__":
     path = '/data/dataprocessing/interproscan/all_bacilli.tsv'
     df = spark_df(path)
-    inspect = InspectInterPRO(df)
+    inspect = InspectSparkDF(df)
     # print(inspect.unique_pro_annots())
     print(inspect.avg_pro_annots())
 
