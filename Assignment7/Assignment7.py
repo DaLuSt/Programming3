82386a00b7ef257a65123d77bb3dcca3faeaa0eb
DaLuSt <daan_lucas@live.nl>
2023-10-01 17:23:45 +0200

    base code template for loading and proces, fail

--- Assignment7/Assignment7.py
+++ Assignment7/Assignment7.py
@@ -3,6 +3,21 @@
 Title: Replacement Assignment
 Author: Daan Steur
 Date: 05/10/2023
-Description: 
+Description: This script parses a PubMed XML file using PySpark and processes the data to obtain key information from research articles.
 Usage: 
 """
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, year, length, avg
+from pyspark.sql.types import ArrayType, StringType
+import csv
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
