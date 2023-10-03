37e86aa900be20e8061e6eb25095d988efb010b0
DaLuSt <daan_lucas@live.nl>
2023-10-03 13:15:58 +0200

    complete working script, refactor and clean needed

--- Assignment7/Assignment7.py
+++ Assignment7/Assignment7.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Title: Replacement Assignment
+Title: Replacement Assignment XML parser
 Author: Daan Steur
 Date: 05/10/2023
 Description: This script parses a PubMed XML file using PySpark and processes the data to obtain key information from research articles.
@@ -8,48 +8,30 @@ Usage: python3 Assignment7.py [file_limit] (file_limit is optional)
 """
 
 import os
-import time
 import sys
 import pandas as pd
 import xml.etree.ElementTree as ET
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, avg
+from pyspark.sql.functions import col, min, max, avg
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 
-
-def create_spark_session(app_name):
-    """
-    Creates a Spark session.
-
-    Args:
-        app_name (str): Name for the Spark application.
-
-    Returns:
-        SparkSession: A Spark session, or None if an error occurs.
-    """
+def create_spark_session(app_name, num_executors=16, executor_cores=16, executor_memory='128g', driver_memory='128g'):
     try:
-        # Create a Spark session with the provided app name
-        spark = SparkSession.builder.appName(app_name).getOrCreate()
+        spark = SparkSession.builder \
+            .appName(app_name) \
+            .config("spark.executor.instances", num_executors) \
+            .config("spark.executor.cores", executor_cores) \
+            .config("spark.executor.memory", executor_memory) \
+            .config("spark.driver.memory", driver_memory) \
+            .getOrCreate()
+        
         return spark
     except Exception as e:
-        # Handle any exceptions that may occur during Spark session creation
         print(f"An error occurred while creating the Spark session: {str(e)}")
         return None
 
 def create_articles_dataframe(spark, input_directory, file_limit=None):
-    """
-    Parses PubMed XML files in the input directory and creates a PySpark DataFrame.
-
-    Args:
-        spark (SparkSession): The Spark session.
-        input_directory (str): Path to the directory containing PubMed XML files.
-        file_limit (int): Limit for the number of files to process (optional).
-
-    Returns:
-        DataFrame: A PySpark DataFrame containing article data, or None if an error occurs.
-    """
     try:
-        # Define the schema for the articles DataFrame
         schema = StructType([
             StructField("PubMedID", StringType(), nullable=True),
             StructField("FirstAuthor", StringType(), nullable=True),
@@ -58,76 +40,69 @@ def create_articles_dataframe(spark, input_directory, file_limit=None):
             StructField("Title", StringType(), nullable=True),
             StructField("JournalTitle", StringType(), nullable=True),
             StructField("Abstract", StringType(), nullable=True),
+            StructField("AbstractLength", IntegerType(), nullable=True),
             StructField("References", StringType(), nullable=True)
         ])
 
-        # Create an empty DataFrame for articles with the defined schema
         articles_df = spark.createDataFrame([], schema=schema)
 
-        # Initialize a counter
         file_count = 0
 
-        # Iterate over XML files in the input directory
         for filename in os.listdir(input_directory):
             if filename.endswith(".xml"):
                 filepath = os.path.join(input_directory, filename)
                 
-                # Parse the XML file
                 tree = ET.parse(filepath)
                 root = tree.getroot()
 
-                # Extract data from XML and create rows
+                article_data = []
+
                 for article in root.findall(".//PubmedArticle"):
-                    pubmed_id = article.find(".//PMID").text
-                    first_author = article.find(".//AuthorList/Author[1]/LastName").text
-                    last_author = article.find(".//AuthorList/Author[last()]/LastName").text
-                    pub_year = article.find(".//PubDate/Year").text if article.find(".//PubDate/Year") is not None else "Unknown"
-                    title = article.find(".//ArticleTitle").text
-                    journal_title = article.find(".//Journal/Title").text
+                    pubmed_id = article.find(".//PMID")
+                    first_author = article.find(".//AuthorList/Author[1]/LastName")
+                    last_author = article.find(".//AuthorList/Author[last()]/LastName")
+                    pub_year = article.find(".//PubDate/Year")
+                    title = article.find(".//ArticleTitle")
+                    journal_title = article.find(".//Journal/Title")
                     abstract = article.find(".//Abstract/AbstractText")
+                    
+                    pubmed_id_text = pubmed_id.text if pubmed_id is not None else "Unknown"
+                    first_author_text = first_author.text if first_author is not None else "Unknown"
+                    last_author_text = last_author.text if last_author is not None else "Unknown"
+                    pub_year_text = pub_year.text if pub_year is not None else "Unknown"
+                    title_text = title.text if title is not None else "Unknown"
+                    journal_title_text = journal_title.text if journal_title is not None else "Unknown"
                     abstract_text = abstract.text if abstract is not None else ""
                     
-                    # Extract references if available
+                    # Calculate abstract length
+                    abstract_length = len(abstract_text)
+
                     references = [ref.text for ref in article.findall(".//PubmedData/ReferenceList/Reference/ArticleIdList/ArticleId[@IdType='pubmed']")]
 
-                    # Append the data to the DataFrame
-                    articles_df = articles_df.union(
-                        spark.createDataFrame([(pubmed_id, first_author, last_author, pub_year, title, journal_title, abstract_text, references)], 
-                                             schema=schema)
-                    )
+                    article_data.append((pubmed_id_text, first_author_text, last_author_text, pub_year_text, title_text, journal_title_text, abstract_text, abstract_length, references))
 
-                    # Increment the file count
-                    file_count += 1
+                articles_df = articles_df.union(
+                    spark.createDataFrame(article_data, schema=schema)
+                )
 
-                    # Check if the file limit has been reached
-                    if file_limit is not None and file_count >= file_limit:
-                        break
-                
-                # Check if the file limit has been reached
+                file_count += 1
+
                 if file_limit is not None and file_count >= file_limit:
                     break
-
+                
         return articles_df
     except Exception as e:
-        # Handle any exceptions that may occur during DataFrame creation
         print(f"An error occurred while creating the DataFrame: {str(e)}")
         return None
 
 def save_dataframe_as_csv(dataframe, output_path):
-    """
-    Saves a PySpark DataFrame as a CSV file.
-
-    Args:
-        dataframe (DataFrame): The DataFrame to be saved.
-        output_path (str): Path to the output CSV file.
-    """
     try:
         dataframe.write.csv(output_path, mode="overwrite", header=True)
         print(f"DataFrame saved as CSV to {output_path}")
     except Exception as e:
         print(f"Error saving DataFrame as CSV: {str(e)}")
-
-
+        
+        
 def create_and_save_analysis_dataframes(articles_df):
     """
     Creates and saves analysis DataFrames based on the provided article DataFrame.
@@ -139,7 +114,14 @@ def create_and_save_analysis_dataframes(articles_df):
         # Create the DataFrame to answer questions
         author_counts = articles_df.groupBy("FirstAuthor").count().alias("ArticleCountPerAuthor")
         year_counts = articles_df.groupBy("Year").count().alias("ArticleCountPerYear")
-        abstract_lengths = articles_df.groupBy().agg(avg(col("Abstract").cast("string").cast(IntegerType())).alias("AvgAbstractLength"))
+        
+        # Calculate min, max, and average of AbstractLength column
+        abstract_lengths = articles_df.agg(
+            min(col("AbstractLength")).alias("MinAbstractLength"),
+            max(col("AbstractLength")).alias("MaxAbstractLength"),
+            avg(col("AbstractLength")).alias("AvgAbstractLength")
+        )
+        
         journal_year_counts = articles_df.groupBy("JournalTitle", "Year").count().alias("ArticleCountPerJournalTitlePerYear")
 
         # Save the DataFrames to CSV files
@@ -153,13 +135,14 @@ def create_and_save_analysis_dataframes(articles_df):
         print(f"Error creating and saving analysis DataFrames: {str(e)}")
 
     
-def combine_csv_files(input_folder, output_file):
+def combine_and_delete_files(input_folder, output_file, combined_csv_filename):
     """
-    Combines multiple CSV files into a single CSV file.
+    Combines multiple CSV files into a single CSV file and deletes all files in a folder except the combined CSV file.
 
     Args:
         input_folder (str): Path to the folder containing CSV files to be combined.
         output_file (str): Path to the output combined CSV file.
+        combined_csv_filename (str): Name of the combined CSV file to be retained.
     """
     try:
         # Get a list of all CSV files in the input folder
@@ -169,55 +152,74 @@ def combine_csv_files(input_folder, output_file):
             print("No CSV files found in the input folder.")
             return
 
-        # Read the first CSV file to get the header
-        first_csv = pd.read_csv(os.path.join(input_folder, csv_files[0]))
-        header = list(first_csv.columns)
+        # Initialize an empty DataFrame to store the combined data
+        combined_df = pd.DataFrame()
 
-        # Create an empty DataFrame to store the combined data
-        combined_df = pd.DataFrame(columns=header)
-
-        # Append data from each CSV file to the combined DataFrame
+        # Iterate over each CSV file and concatenate them
         for csv_file in csv_files:
             csv_path = os.path.join(input_folder, csv_file)
-            df = pd.read_csv(csv_path)
-            combined_df = pd.concat([combined_df, df], ignore_index=True)
+            try:
+                df = pd.read_csv(csv_path)
+                combined_df = pd.concat([combined_df, df], ignore_index=True)
+            except pd.errors.ParserError:
+                print(f"Skipping file {csv_file} due to parsing error.")
 
         # Save the combined DataFrame to the output CSV file
         combined_df.to_csv(output_file, index=False)
         print(f"Combined data saved to {output_file}")
-    except Exception as e:
-        print(f"Error combining CSV files: {str(e)}")
 
-    
-def delete_files_except_combined_csv(folder_path, combined_csv_filename):
-    """
-    Deletes all files in a folder except the combined CSV file.
-
-    Args:
-        folder_path (str): Path to the folder containing files to be deleted.
-        combined_csv_filename (str): Name of the combined CSV file to be retained.
-    """
-    try:
-        for filename in os.listdir(folder_path):
+        # Delete all files in the folder except the combined CSV file
+        for filename in os.listdir(input_folder):
             if filename != combined_csv_filename:
-                file_path = os.path.join(folder_path, filename)
+                file_path = os.path.join(input_folder, filename)
                 if os.path.isfile(file_path):
                     os.remove(file_path)
         print(f"Deleted all files except {combined_csv_filename}")
     except Exception as e:
-        print(f"An error occurred while deleting files: {str(e)}")
+        print(f"Error combining and deleting files: {str(e)}")
 
-
-def main(input_directory, file_limit=None):
+def csv_name_change_logfile_delete(folder, new_name, confirm_logfile_delete="y"):
     """
-    Main function to execute the PubMed XML data parsing and processing.
+    Rename CSV files in the specified folder to the new name and optionally delete other files.
 
     Args:
-        input_directory (str): Path to the directory containing PubMed XML files.
-        file_limit (int): Limit for the number of files to process (optional).
+        folder (str): The path to the folder containing CSV files.
+        new_name (str): The new name to assign to CSV files.
+        confirm_logfile_delete (str, optional): A confirmation prompt for deleting other files.
+            Defaults to "y". Set to "n" to skip deletion.
+
+    Returns:
+        None
     """
+    for filename in os.listdir(folder):
+        if filename.endswith(".csv"):
+            old_path = os.path.join(folder, filename)
+            new_path = os.path.join(folder, new_name)
+            os.rename(old_path, new_path)
+            print(f"Renamed {old_path} to {new_path}")
+
+    # Ask the user for confirmation before deleting files
+    if confirm_logfile_delete == "y":
+        for filename in os.listdir(folder):
+            file_path = os.path.join(folder, filename)
+            if os.path.isfile(file_path) and filename != new_name:
+                os.remove(file_path)
+        print(f"Deleted all files except {new_name}")
+    else:
+        print("Files were not deleted.")
+
+
+def main(input_directory, file_limit = 1):
     try:
-        time_start = time.time()
+        # create output folder if it does not exist
+        if not os.path.exists("output"):
+            os.makedirs("output")
+
+        # Define folder paths and filenames
+        parsed_data_folder = "output/parsed_data"
+        output_file = os.path.join(parsed_data_folder, "combined_data.csv")
+        combined_csv_filename = "combined_data.csv"
+
         # Initialize a Spark session
         spark = create_spark_session("PubMedXMLParser")
 
@@ -225,32 +227,28 @@ def main(input_directory, file_limit=None):
         articles_df = create_articles_dataframe(spark, input_directory, file_limit)
 
         # Save the entire DataFrame to a single CSV file
-        save_dataframe_as_csv(articles_df, "output/parsed_data")
+        save_dataframe_as_csv(articles_df, parsed_data_folder)
 
         # Create and save analysis DataFrames
         create_and_save_analysis_dataframes(articles_df)
+
+        # Combine output CSV files and delete all files except the combined CSV file
+        combine_and_delete_files(parsed_data_folder, output_file, combined_csv_filename)
+        print(f"Combined data saved to {output_file}.")
         
-        input_folder = "output/parsed_data"
-        output_file = "output/parsed_data/combined_data.csv"
-        combine_csv_files(input_folder, output_file)
-        
-        
-        combined_csv_filename = "combined_data.csv"
-        delete_files_except_combined_csv(input_folder, combined_csv_filename)
+        # Rename the CSV files and delete all files except the renamed CSV files
+        csv_name_change_logfile_delete("output/abstract_lengths", "abstract_lengths.csv", confirm_logfile_delete="y")
+        csv_name_change_logfile_delete("output/author_counts", "author_counts.csv", confirm_logfile_delete="y")
+        csv_name_change_logfile_delete("output/journal_year_counts", "journal_year_counts.csv", confirm_logfile_delete="y")
+        csv_name_change_logfile_delete("output/year_counts", "year_counts.csv", confirm_logfile_delete="y")
 
         # Stop the Spark session
         spark.stop()
-        time_stop = time.time()
-        print(f"Total execution time: {time_stop - time_start} seconds")
-        
     except Exception as e:
         print(f"An error occurred in the main function: {str(e)}")
-    
 
 if __name__ == "__main__":
-    input_folder = "output/parsed_data"
-    input_dir = "/data/datasets/NCBI/PubMed/"
-    
+
     # Check if the file limit is provided as a command line argument
     if len(sys.argv) > 1:
         try:
@@ -261,5 +259,5 @@ if __name__ == "__main__":
     else:
         # If no file limit is provided, set it to None to parse all files
         file_limit = None
-        
-    main(input_dir, file_limit)
+
+    main("/data/datasets/NCBI/PubMed/", file_limit)
